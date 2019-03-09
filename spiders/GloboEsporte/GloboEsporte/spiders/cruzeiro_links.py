0bf525184035307f4c7e92fc5c46c754d6c3d125
Júlio César <juliocmalvares07@gmail.com>
2019-03-09 18:56:31 -0300

    spider creation to capture links

--- spiders/GloboEsporte/GloboEsporte/spiders/cruzeiro_links.py
+++ spiders/GloboEsporte/GloboEsporte/spiders/cruzeiro_links.py
@@ -4,15 +4,24 @@ import scrapy
 
 class CruzeiroLinksSpider(scrapy.Spider):
     name = 'cruzeiro_links'
-    start_urls = ['http://globoesporte.globo.com/futebol/times/cruzeiro/index/feed/pagina-1.ghtml/']
+    start_urls = ['https://globoesporte.globo.com/futebol/times/cruzeiro/index/feed/pagina-1.ghtml']
     links = []
     counter = 1
+    arch = 'cruzeiro_links.csv'
     def parse(self, response):
         dlinks = response.xpath('.//a[contains(@class, "feed-post-link")]/@href').extract()
         for l in dlinks:
             self.links.append(l)
         self.counter+=1
+        print(" >>> COUNTER:", self.counter)
+
+        if self.counter % 10 == 0:
+            fl = open(self.arch, 'a+')
+            for link in self.links:
+                fl.write(link + ',\n')
+            self.links.clear()
+            fl.close()
         yield scrapy.Request(
-            url='http://globoesporte.globo.com/futebol/times/cruzeiro/index/feed/pagina-%i.ghtml/' % self.counter,
+            url='https://globoesporte.globo.com/futebol/times/cruzeiro/index/feed/pagina-%i.ghtml' % self.counter,
             callback=self.parse
         )
