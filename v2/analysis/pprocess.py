1be04e4746c5aabe3adee0ded6d6e2ca0d5dac24
Julio Cesar <juliocmalvares07@gmail.com>
2020-05-28 16:23:04 -0300

    corrections

--- v2/analysis/pprocess.py
+++ v2/analysis/pprocess.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import nltk
 from joblib import Parallel, delayed
 import time
 import pandas
-from tqdm import tqdm
 from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
 
 class preprocessing(object):
